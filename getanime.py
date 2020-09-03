8417b8d881c4964b96808e5e634e0f537aa08958
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-09-03 20:55:33 +0300

    Update getanime.py

--- getanime.py
+++ getanime.py
@@ -1,57 +1,59 @@
-import requests, json, json
-from bs4 import BeautifulSoup
+import requests
 from io import StringIO
+import json, re
 
+def search_anilist(search, max_results=10):
+	query = """
+	query ($id: Int, $page: Int, $search: String, $type: MediaType) {
+		Page (page: $page, perPage: 10) {
+			media (id: $id, search: $search, type: $type) {
+				id
+				idMal
+				description(asHtml: false)
+				title {
+					english
+					romaji
+				}
+				coverImage {
+					extraLarge
+				}
+				bannerImage
+				averageScore
+				meanScore
+				status
+				genres
+				episodes
+				externalLinks {
+					url
+					site
+				}
+				nextAiringEpisode {
+					timeUntilAiring
+				}
+			}
+		}
+	}
+	"""
+	variables = {
+		'search': search,
+		'page': 1,
+		'perPage': max_results,
+		'type': 'ANIME',
+		'sort': 'SEARCH_MATCH'
+	}
+	url = 'https://graphql.anilist.co'
 
-def search(search):
-  query = '''
-  query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-      Page (page: $page, perPage: $perPage) {
-          pageInfo {
-              total
-              currentPage
-              lastPage
-              hasNextPage
-              perPage
-          }
-          media (id: $id, search: $search, type: ANIME, sort: SEARCH_MATCH) {
-              id
-              title {
-                  romaji
-              }
-          }
-      }
-  }
-  '''
-  variables = {
-      'search': search,
-      'page': 1,
-      'perPage': 1,
-      'type': 'ANIME'
-  }
-  url = 'https://graphql.anilist.co'
+	response = requests.post(url, json={'query': query, 'variables': variables})
+	io = StringIO(response.text)
+	results = json.load(io)['data']['Page']['media'][0]
 
-  response = requests.post(url, json={'query': query, 'variables': variables})
-  io = StringIO(response.text)
-  results = json.load(io)
-  result_list = results['data']['Page']['media']
-  final_result = []
-  title = result_list[0]['title']['romaji']
-  return title
-
-
-
-def gogo_api_ani_list(method, query):
-	romaji = search(query)
-	base_url = f'https://gogoanime.now.sh/api/v1/{method + romaji}'.lower().replace(' ', '%20')
-	io = StringIO(requests.get(f'{base_url}').text)
-	io = json.load(io)
-	return io
-
-
-def gogo_api(method, query):
-	base_url = f'https://gogoanime.now.sh/api/v1/{method + query}'.lower().replace(' ', '%20')
-	io = StringIO(requests.get(f'{base_url}').text)
-	io = json.load(io)
-	return io
-
+	res = {
+		'desc': re.sub(r"\<(.*?)\>", '', results['description']),
+		'title': results['title']['english'],
+		'link': 'https://anilist.co/anime/{}'.format(results['id']),
+		'img': results['coverImage']['extraLarge'],
+		'status': results['status'],
+		'genres': results['genres'],
+		'totalEpisodes': results['episodes']
+		}
+	return res
